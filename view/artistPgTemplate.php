29759618a230522333096a1f617191dea2dd5f52
Olivia M Steffenhagen <oms4556@banjo.rit.edu>
2025-03-27 11:49:17 -0400

    css/html for template

--- view/artistPgTemplate.php
+++ view/artistPgTemplate.php
@@ -13,7 +13,7 @@
     <div class="side">
         <div>
             <h4>Music Type</h4>
-            <p>Covers/parodies/original music</p>
+            <p>Covers, parodies, and/or original music</p>
         </div>
         <div>
             <h4>Publishers</h4>
@@ -25,7 +25,7 @@
         </div>
         <div>
             <h4>Connections</h4>
-            <p>Connection name, connection name...</p>
+            <p>Connection name, connection name</p>
         </div>
     </div>
     <div class="bottomContainer">
@@ -42,55 +42,57 @@
         
     </div>
 </div>
-<div>
-    <h2>About [Artist name]</h2>
-    <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-    sed do eiusmod tempor incididunt ut labore et dolore 
-    magna aliqua. Ut enim ad minim veniam, quis nostrud 
-    exercitation ullamco laboris nisi ut aliquip ex ea 
-    commodo consequat. Duis aute irure dolor in reprehenderit 
-    in voluptate velit esse cillum dolore eu fugiat nulla 
-    pariatur. Excepteur sint occaecat cupidatat non proident, 
-    sunt in culpa qui officia deserunt mollit anim id est 
-    laborum.
-    <br>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-    sed do eiusmod tempor incididunt ut labore et dolore 
-    magna aliqua. Ut enim ad minim veniam, quis nostrud 
-    exercitation ullamco laboris nisi ut aliquip ex ea 
-    commodo consequat. Duis aute irure dolor in reprehenderit 
-    in voluptate velit esse cillum dolore eu fugiat nulla 
-    pariatur. Excepteur sint occaecat cupidatat non proident, 
-    sunt in culpa qui officia deserunt mollit anim id est 
-    laborum.
-    </p>
-</div>
-<div class="galleryContainer">
-    <h2>Gallery</h2>
-    <div class="gallery">
-        <figure id="img">
-        <a target="_blank" href="../assets/images/placeholder_img.jpg"><img src="../assets/images/placeholder_img.jpg" alt="temp img" width="300px"></a>
-        <figcaption>image caption(credits)</figcaption>
-        </figure>
-        <figure id="img">
-            <a target="_blank" href="../assets/images/placeholder_img.jpg"><img src="../assets/images/placeholder_img.jpg" alt="temp img" width="300px"></a>
-            <figcaption>image caption(credits)</figcaption>
-        </figure>
-        <figure id="img">
-            <a target="_blank" href="../assets/images/placeholder_img.jpg"><img src="../assets/images/placeholder_img.jpg" alt="temp img" width="300px"></a>
-            <figcaption>image caption(credits)</figcaption>
-        </figure>
+<div class="bottomContent">
+    <div>
+        <h2>About [Artist name]</h2>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore 
+        magna aliqua. Ut enim ad minim veniam, quis nostrud 
+        exercitation ullamco laboris nisi ut aliquip ex ea 
+        commodo consequat. Duis aute irure dolor in reprehenderit 
+        in voluptate velit esse cillum dolore eu fugiat nulla 
+        pariatur. Excepteur sint occaecat cupidatat non proident, 
+        sunt in culpa qui officia deserunt mollit anim id est 
+        laborum.
+        <br>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore 
+        magna aliqua. Ut enim ad minim veniam, quis nostrud 
+        exercitation ullamco laboris nisi ut aliquip ex ea 
+        commodo consequat. Duis aute irure dolor in reprehenderit 
+        in voluptate velit esse cillum dolore eu fugiat nulla 
+        pariatur. Excepteur sint occaecat cupidatat non proident, 
+        sunt in culpa qui officia deserunt mollit anim id est 
+        laborum.
+        </p>
     </div>
-    
+    <div class="galleryContainer">
+        <h2>Gallery</h2>
+        <div class="gallery">
+            <figure id="img">
+            <a target="_blank" href="../assets/images/placeholder_img.jpg"><img src="../assets/images/placeholder_img.jpg" alt="temp img" width="300px"></a>
+            <figcaption>image caption(credits)</figcaption>
+            </figure>
+            <figure id="img">
+                <a target="_blank" href="../assets/images/placeholder_img.jpg"><img src="../assets/images/placeholder_img.jpg" alt="temp img" width="300px"></a>
+                <figcaption>image caption(credits)</figcaption>
+            </figure>
+            <figure id="img">
+                <a target="_blank" href="../assets/images/placeholder_img.jpg"><img src="../assets/images/placeholder_img.jpg" alt="temp img" width="300px"></a>
+                <figcaption>image caption(credits)</figcaption>
+            </figure>
+        </div>
+        
+    </div>
+    <div class="links">
+        <h3>Social Media/Website Links</h3>
+        <ul>
+            <li><a href="#">Link 1</a></li>
+            <li><a href="#">Link 2</a></li>
+            <li><a href="#">Link 3</a></li>
+        </ul>
+    </div>
+    <a href="#header" class="backToTop">Back to top</a>
 </div>
-<div>
-    <h3>Social Media/Website Links</h3>
-    <a href="#">Link 1</a>
-    <br>
-    <a href="#">Link 2</a>
-    <br>
-    <a href="#">Link 3</a>
-</div>
-<a href="#header">Back to top</a>
 <?php include("../view/include/footer.php"); ?>
